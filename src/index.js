161ecf020c4260a8af86aa4fa0466ff72fe5090c
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-08-04 22:43:24 +0300

    barely working version :)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 'use strict';
 
 import './guard';
-import fs from 'fs';
 import hook from './hook';
 import postcss from 'postcss';
+import { basename, dirname, join, resolve } from 'path';
+import { readFileSync } from 'fs';
 
 import extractImports from 'postcss-modules-extract-imports';
 import localByDefault from 'postcss-modules-local-by-default';
 import scope from 'postcss-modules-scope';
+import parser from './parser';
 
 let plugins = [localByDefault, extractImports, scope];
 
-hook(filename => postcss(plugins).process(fs.readFileSync(filename, 'utf8')).css);
+const load = (sourceString, sourcePath, trace, pathFetcher) => {
+  let exportTokens = {};
+  let result = postcss(plugins.concat(new parser({ exportTokens, pathFetcher, trace })))
+    .process(sourceString, {from: '/' + sourcePath})
+    .stringify();
+
+  return { injectableSource: result.css, exportTokens: exportTokens };
+}
+
+hook(filename => {
+  const root = dirname(filename);
+  const sources = {};
+  const tokensByFile = {};
+  let importNr = 0;
+
+  const fetch = (_newPath, _relativeTo, _trace) => {
+    let newPath = _newPath.replace(/^["']|["']$/g, '');
+    let trace = _trace || String.fromCharCode(importNr++);
+
+    let relativeDir = dirname(_relativeTo);
+    let rootRelativePath = resolve(relativeDir, newPath);
+    let fileRelativePath = resolve(join(root, relativeDir), newPath);
+
+    const tokens = tokensByFile[fileRelativePath];
+    if (tokens) {
+      return tokens;
+    }
+
+    let source = readFileSync(fileRelativePath, 'utf-8');
+    let { injectableSource, exportTokens } = load(source, rootRelativePath, trace, fetch);
+
+    sources[trace] = injectableSource;
+    tokensByFile[fileRelativePath] = exportTokens;
+
+    return exportTokens;
+  }
+
+  return fetch(basename(filename), '/');
+});
 
 /**
  * @param  {object} opts
