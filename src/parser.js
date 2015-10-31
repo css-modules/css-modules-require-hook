94e21895454621ee35ec6c6a9a3eac840d78d144
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-10-31 23:06:09 +0300

    simplified version of the parser plugin

--- src/parser.js
+++ src/parser.js
@@ -1,60 +1,38 @@
 import { plugin } from 'postcss';
+import forEach from 'lodash.foreach';
 import replaceSymbols from 'icss-replace-symbols';
-
 const importRegexp = /^:import\((.+)\)$/;
+const exportRegexp = /^:export$/;
 
-export default plugin('parser', function parser(opts = {}) {
-  const exportTokens = {};
-  const translations = {};
-
-  const fetchImport = (importNode, relativeTo, depNr) => {
-    const file = importNode.selector.match(importRegexp)[1];
-    const depTrace = opts.trace + String.fromCharCode(depNr);
-    const exports = opts.fetch(file, opts.filename, depTrace);
-
-    importNode.each(decl => {
-      if (decl.type === 'decl') {
-        translations[decl.prop] = exports[decl.value];
-      }
-    });
-
-    importNode.removeSelf();
-  };
-
-  const fetchAllImports = css => {
-    let imports = 0;
-
-    css.each(node => {
-      if (node.type === 'rule' && node.selector.match(importRegexp)) {
-        fetchImport(node, css.source.input.from, imports++);
-      }
-    });
-  };
-
-  const linkImportedSymbols = css => replaceSymbols(css, translations);
-
-  const handleExport = exportNode => {
-    exportNode.each(decl => {
-      if (decl.type === 'decl') {
-        Object.keys(translations).forEach(translation => {
-          decl.value = decl.value.replace(translation, translations[translation]);
-        });
-
-        exportTokens[decl.prop] = decl.value;
-      }
-    });
-
-    exportNode.removeSelf();
-  };
-
-  const extractExports = css => css.each(node => {
-    if (node.type === 'rule' && node.selector === ':export') handleExport(node);
-  });
-
+/**
+ * @param  {function} options.fetch
+ * @return {function}
+ */
+export default plugin('parser', function parser({ fetch } = {}) {
   return css => {
-    fetchAllImports(css);
-    linkImportedSymbols(css);
-    extractExports(css);
+    // https://github.com/postcss/postcss/blob/master/docs/api.md#inputfile
+    const file = css.source.input.file;
+    const translations = {};
+    const exportTokens = {};
+
+    css.walkRules(importRegexp, rule => {
+      const exports = fetch(RegExp.$1, file);
+
+      rule.walkDecls(decl => translations[decl.prop] = exports[decl.value]);
+      rule.remove();
+    });
+
+    replaceSymbols(css, translations);
+
+    css.walkRules(exportRegexp, rule => {
+      rule.walkDecls(decl => {
+        forEach(translations, (value, key) => decl.value = decl.value.replace(key, value));
+        exportTokens[decl.prop] = decl.value;
+      });
+
+      rule.remove();
+    });
+
     css.tokens = exportTokens;
   };
 });
