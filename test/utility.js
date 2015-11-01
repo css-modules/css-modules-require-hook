e335520d2115f23edadeec28661745232cf76e33
Alexey Litvinov <iamhoneyboy@gmail.com>
2015-11-01 14:38:44 +0300

    deadcode elimination

--- test/utility.js
+++ test/utility.js
@@ -1,19 +1,7 @@
 import { ok, equal, throws } from 'assert';
-import { get, is, removeQuotes } from '../src/utility';
+import { is, removeQuotes } from '../src/utility';
 
 describe('utility', () => {
-  describe('get()', () => {
-    it('should return value for an existing property', () => equal(get('a', null, 'string', {a: 'val'}), 'val'));
-
-    it('should return value for an existing alias', () => equal(get('a', ['b'], 'string', {b: 'val'}), 'val'));
-
-    it('should return null for a non-existing property', () => equal(get('a', null, 'array', {}), null));
-
-    it('should throw an error for the specified key with wrong type of value', () => {
-      throws(() => get('a', null, 'string', {a: 5}));
-    });
-  });
-
   describe('is()', () => {
     it('should return true for an array', () => ok(is('array', [])));
 
