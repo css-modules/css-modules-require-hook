42f5488109e6a97e843b029bd5aad5158c154896
Alexey Litvinov <iamhoneyboy@gmail.com>
2016-02-19 02:46:46 +0300

    cleanup

--- preset.js
+++ preset.js
@@ -1,7 +1,7 @@
 const hook = require('.');
 const lookup = require('lookup-fs');
 
-lookup('css-modules-file.js', (err, cssModulesFilePath) => {
+lookup('cmrh.conf.js', (err, cssModulesFilePath) => {
   if (err) {
     throw new Error('Unable to find css-modules-file.js');
   }
--- src/guard.js
+++
@@ -1,5 +0,0 @@
-if (global._cssModulesPolyfill) {
-  throw new Error('only one instance of css-modules-require-hook is allowed');
-}
-
-global._cssModulesPolyfill = true;
