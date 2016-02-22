13f1f01183c131e6355c3cf1bcac4cefbd1eadd3
Alexey Litvinov <sullenor@yandex-team.ru>
2016-02-23 00:22:15 +0300

    preset update

--- preset.js
+++ preset.js
@@ -1,15 +1,15 @@
+const basename = require('path').basename;
 const debug = require('debug')('css-modules:preset');
 const dirname = require('path').dirname;
-const hook = require('.');
+const hook = require('./lib/index');
 const seekout = require('seekout');
 
-debug('→ cmrh.conf.js');
 const preset = seekout('cmrh.conf.js', dirname(module.parent.filename));
 
-if (!preset) {
-  debug('failure');
-  hook({});
-} else {
-  debug('success');
+if (preset) {
+  debug(`→ ${basename(preset)}`);
   hook(require(preset));
+} else {
+  debug(`→ defaults`);
+  hook({});
 }
--- test/preset/index.js
+++ test/preset/index.js
@@ -2,17 +2,33 @@ const detachHook = require('../sugar').detachHook;
 const dropCache = require('../sugar').dropCache;
 
 suite('css-modules-require-hook/preset', () => {
-  setup(() => require('../../preset'));
+  suite('using cmrh.conf.js file', () => {
+    test('should return tokens', () => {
+      const tokens = require('./fixture/oceanic.css');
 
-  test('should return tokens', () => {
-    const tokens = require('./fixture/oceanic.css');
-    assert.deepEqual(tokens, {
-      color: 'oceanic__color___1sqWL',
+      assert.deepEqual(tokens, {
+        color: 'oceanic__color___1sqWL',
+      });
     });
+
+    setup(() => require('./js/preset'));
+  });
+
+  suite('using defaults', () => {
+    test('should return tokens', () => {
+      const tokens = require('./fixture/oceanic.css');
+
+      assert.deepEqual(tokens, {
+        color: '_test_preset_fixture_oceanic__color',
+      });
+    });
+
+    setup(() => require('../../preset'));
   });
 
   teardown(() => {
     detachHook('.css');
+    dropCache('../preset');
     dropCache('./preset/fixture/oceanic.css');
   });
 });
