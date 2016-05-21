05089ab3495997b8563f7896f8428ef7204c360d
Taz Jackson <tatjsn@users.noreply.github.com>
2016-05-21 23:38:21 +0900

    Introduce option adding camelize keys to token

---
+++ test/api/camelCase.js
@@ -0,0 +1,20 @@
+const detachHook = require('../sugar').detachHook;
+const dropCache = require('../sugar').dropCache;
+const identity = require('lodash').lodash;
+
+suite('api/camelCase', () => {
+  test('should add camel case keys in token', () => {
+    const tokens = require('./fixture/bem.css');
+    assert.deepEqual(tokens, {
+      blockElementModifier: '_test_api_fixture_bem__block__element--modifier',
+      'block__element--modifier': '_test_api_fixture_bem__block__element--modifier',
+    });
+  });
+
+  setup(() => hook({ camelCase: true }));
+
+  teardown(() => {
+    detachHook('.css');
+    dropCache('./api/fixture/bem.css');
+  });
+});
